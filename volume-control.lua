7342c2cb0dc2d2ec7ffe2a1dfcab151ad17c29a9
Thomas Gläßle <t_glaessle@gmx.de>
2013-08-11 02:01:35 +0200

    Generate command line from table
    Use argv list instead of concating all arguments manually.

--- volume-control.lua
+++ volume-control.lua
@@ -9,13 +9,37 @@ local vcontrol = { mt = {}, wmt = {} }
 vcontrol.wmt.__index = vcontrol
 
 
-function readcommand(command)
+------------------------------------------
+-- Private utility functions
+------------------------------------------
+
+local function readcommand(command)
     local file = io.popen(command)
     local text = file:read('*all')
     file:close()
     return text
 end
 
+local function quote(str)
+    return "'" .. string.gsub(str, "'", "'\\''") .. "'"
+end
+
+local function arg(first, ...)
+    if #{...} == 0 then
+        return quote(first)
+    else
+        return quote(first), arg(...)
+    end
+end
+
+local function argv(...)
+    return table.concat({arg(...)}, " ")
+end
+
+
+------------------------------------------
+-- Volume control interface
+------------------------------------------
 
 function vcontrol.new(args)
     local sw = setmetatable({}, vcontrol.wmt)
@@ -23,6 +47,7 @@ function vcontrol.new(args)
     sw.cmd = "amixer"
     sw.cardid  = args.cardid or nil
     sw.channel = args.channel or "Master"
+    sw.step = args.step or '5%'
 
     sw.widget = wibox.widget.textbox()
     sw.widget.set_align("right")
@@ -59,32 +84,32 @@ function vcontrol:mixercommand(command)
     self.widget:set_text(volume .. " ")
 end
 
-function vcontrol:mkcmd(command)
+function vcontrol:mkcmd(...)
     if self.cardid == nil then
-        return "amixer " .. command
+        return argv("amixer", ...)
     else
-        return "amixer -c " .. self.cardid .. " " .. command
+        return argv("amixer", "-c", self.cardid, ...)
     end
 end
 
 function vcontrol:update()
-    self:mixercommand(self:mkcmd("get " .. self.channel))
+    self:mixercommand(self:mkcmd("get", self.channel))
 end
 
 function vcontrol:up()
-    self:mixercommand(self:mkcmd("set " .. self.channel .. " 5%+"))
+    self:mixercommand(self:mkcmd("set", self.channel, self.step .. "+"))
 end
 
 function vcontrol:down()
-    self:mixercommand(self:mkcmd("set " .. self.channel .. " 5%-"))
+    self:mixercommand(self:mkcmd("set", self.channel, self.step .. "-"))
 end
 
 function vcontrol:toggle()
-    self:mixercommand(self:mkcmd("set " .. self.channel .. " toggle"))
+    self:mixercommand(self:mkcmd("set", self.channel, "toggle"))
 end
 
 function vcontrol:mute()
-    self:mixercommand(self:mkcmd("set Master mute"))
+    self:mixercommand(self:mkcmd("set", "Master", "mute"))
 end
 
 function vcontrol.mt:__call(...)
