7c7b8796c2594e6895eb8092df347fee0a9f78fd
Thomas Gläßle <t_glaessle@gmx.de>
2017-02-02 10:16:45 +0100

    Start pavucontrol on right-click

--- volume-control.lua
+++ volume-control.lua
@@ -54,7 +54,7 @@ function vcontrol.new(args)
 
     sw.widget:buttons(awful.util.table.join(
         awful.button({}, 1, function() sw:toggle() end),
-        awful.button({}, 3, function() sw:toggle() end),
+        awful.button({}, 3, function() awful.spawn("pavucontrol") end),
         awful.button({}, 4, function() sw:up() end),
         awful.button({}, 5, function() sw:down() end)
     ))
